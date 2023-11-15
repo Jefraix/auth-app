0cc8393e5d792016fc576379604ca659007fd158
juanaguileraht <JAguilera@heat-timer.com>
2023-11-15 16:40:00 -0500

    Lesson 176 & 177

---
+++ util/auth.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const BASE_URL = "https://identitytoolkit.googleapis.com/v1"
+const API_KEY = "AIzaSyC8ZvttVcArHJglM540p_2j_sQC8ErnsDw"
+
+export const createUser = async (email, password) => {
+  const response = await axios.post(
+    BASE_URL + `accounts:signUp?key=${API_KEY}`,
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    }
+  );
+}
