8a519fb36a8e23b976e7e86cc7bf62b1ff375266
Juan Aguilera <jefraix@gmail.com>
2023-11-15 19:11:33 -0500

    Lesson 178 & 179

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,24 +1,32 @@
-import { useState } from 'react';
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
 
-import { createUser } from '../util/auth';
+import { createUser } from "../util/auth";
 
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
 
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   const signupHandler = async ({ email, password }) => {
     setIsAuthenticating(true);
-    await createUser(email, password);
+    try {
+      await createUser(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed!",
+        "Could not create user, please check your input and try again later!"
+      );
+    }
     setIsAuthenticating(false);
-  }
+  };
 
   if (isAuthenticating) {
-    return <LoadingOverlay message="Creating user..." />
+    return <LoadingOverlay message="Creating user..." />;
   }
 
-  return <AuthContent onAuthenticate={signupHandler}/>;
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
--- util/auth.js
+++ util/auth.js
@@ -1,15 +1,24 @@
 import axios from "axios";
 
-const BASE_URL = "https://identitytoolkit.googleapis.com/v1";
+const BASE_URL_ACCOUNTS = "https://identitytoolkit.googleapis.com/v1/accounts:";
 const API_KEY = "AIzaSyC8ZvttVcArHJglM540p_2j_sQC8ErnsDw";
 
+const authenticate = async (mode, email, password) => {
+  const url = BASE_URL_ACCOUNTS + mode + `?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  console.log(response.data);
+};
+
 export const createUser = async (email, password) => {
-  const response = await axios.post(
-    BASE_URL + `/accounts:signUp?key=${API_KEY}`,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+  await authenticate("signUp", email, password);
+};
+
+export const login = async (email, password) => {
+  await authenticate("signInWithPassword", email, password);
 };
